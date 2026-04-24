f145f0108277b2ee818ddf612d0af9662402bc29
noemidepain <127134784+noetdemie@users.noreply.github.com>
2026-04-24 14:01:58 +0200

    Dépôt rendu exercice 4 du TP 2

--- src/main/java/org/sebsy/demo/escaperooms/bll/Room1Service.java
+++ src/main/java/org/sebsy/demo/escaperooms/bll/Room1Service.java
@@ -1,5 +1,10 @@
 package org.sebsy.demo.escaperooms.bll;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("passage")
 public class Room1Service implements RoomService {
 
     public String entreeSalle() {
